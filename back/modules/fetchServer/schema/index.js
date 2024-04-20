9a4e58c0550e894c64d931044cb949447bca49bf
bobr0vv <bota.domoi7@gmail.com>
2024-04-20 19:38:04 +0300

    tarifs done

--- back/modules/fetchServer/schema/index.js
+++ back/modules/fetchServer/schema/index.js
@@ -1,8 +1,8 @@
-import brands from "./brands.js";
+import tarifs from "./tarifs.js";
 import menu from "./menu.js";
 import models from "./models.js";
 import owners from "./owners.js";
 import cards from "./cards.js";
 import services from './services.js';
 
-export default {brands, menu, models, owners, cards, services};
+export default {tarifs, menu, models, owners, cards, services};
---
+++ back/modules/fetchServer/schema/tarifs.js
@@ -0,0 +1,43 @@
+const tarifs = {
+    _id: {},
+        TITLE: {
+            type: 'String',
+            require: true,
+            default: 'None',
+            loc: "Название",
+            sort: true,
+            editable: true,
+            searchable: true,
+            //list: []
+        },
+        TYPES: {
+            type: 'String',
+            require: true,
+            default: 'None',
+            loc: "Типы",
+            sort: true,
+            editable: true,
+            searchable: true,
+        },
+        TIME: {
+            type: 'String',
+            require: false,
+            default: 'None',
+            loc: "Время установки",
+            sort: true,
+            editable: true,
+            searchable: true,
+        },
+        PRICE: {
+            type: 'Number',
+            require: true,
+            default: 0,
+            loc: "Цена в рублях (₽)",
+            sort: true,
+            editable: true,
+            step: 10000,
+            filter: true,
+        }
+    };
+
+    export default tarifs;
