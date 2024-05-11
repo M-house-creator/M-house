2a854e752b0db7c3747f377f512fe3cbd3ac74c5
Vanches999 <151385757+Vanches999@users.noreply.github.com>
2024-05-11 19:34:20 +0300

    ыцыцвцв
    цвцвцв

--- back/modules/fetchServer/api/Controll.js
+++ back/modules/fetchServer/api/Controll.js
@@ -6,27 +6,7 @@ export default class Controll {
     constructor(collectionName = '') {
         this.pathSchema = collectionName;
         this.schema = schema[collectionName];
-    }
-
-    showError(arr) {
-
-    }
-
-    preparePost(query = {}) {
-        let data = {};
-
-        if(query._id) {
-            data._id = new ObjectId(query._id);
-        }
-
-        if(Object.keys(query).length > 0 && Object.keys(this.schema).length > 0) {
-            for(let i in this.schema) {
-                if(i === '_id') continue;
-
-                let checkElement = query[i];
-                let checkSchema = this.schema[i];
-                
-                if(checkElement != '') {
+    
                    switch(checkSchema.type) {
                     case "Number":
                         data[i] = parseFloat(checkElement);
